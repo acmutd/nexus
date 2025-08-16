65a2854e1cfe74e5c9a1b493af14e171c5696b8b
Humza Qureshi <humza@Humzas-MacBook-Pro.local>
2025-08-16 14:53:51 -0500

    test

--- nexus/src/pages/Signup.jsx
+++ nexus/src/pages/Signup.jsx
@@ -214,27 +214,7 @@ export default function Signup() {
           </button>
         </div>
 
-
-          {/* <div className="mb-4">
-            <label
-              htmlFor="confirm_password"
-              className="block text-left text-blue-700 mb-2 font-semibold"
-            >
-              Confirm Password
-            </label>
-            <input
-              id="confirm_password"
-              type={pwVisible ? "text" : "password"}
-              placeholder="Confirm password"
-              className="w-full bg-white text-black px-4 py-2 border border-gray-300 rounded-md focus:outline-none placeholder-gray-400"
-              value={pw2}
-              onChange={(e) => setPw2(e.target.value)}
-              autoComplete="new-password"
-              required
-            />
-            
-          </div> */}
-           <div className="mb-4 relative">
+        <div className="mb-4 relative">
           <label
             htmlFor="confirm_password"
             className="block text-left text-blue-700 mb-2 font-semibold"
