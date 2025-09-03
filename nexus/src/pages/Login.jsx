e167d65938da80daf20ca961709adc0bb277aeb5
Humza Qureshi <humza@Humzas-MacBook-Pro.local>
2025-09-03 11:22:51 -0500

    scrolling login disappear resolved

--- nexus/src/pages/Login.jsx
+++ nexus/src/pages/Login.jsx
@@ -188,7 +188,7 @@ const Login = () => {
           <div className="mb-3">
             <button
               type="submit"
-              className="w-full bg-blue-600 text-white rounded-md py-2 px-4 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-base"
+              className="w-full bg-black text-white rounded-md py-2 px-4 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-base"
             >
               Login
             </button>
