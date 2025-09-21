84f37da3fc14a935c99f553463396bdacbad86ef
YesMyNameIsMe <nguyen424787@gmail.com>
2025-09-20 22:22:11 -0500

    Access Request Page Touch-Ups
    Minor formatting changes to the Access Request Pages

--- nexus/src/pages/accessrequest.jsx
+++ nexus/src/pages/accessrequest.jsx
@@ -10,34 +10,34 @@ function AccessRequest() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-nexus800 bg-cover bg-center"
       style={{ backgroundImage: isMed ? "url('/assets/AccessRequestBGLong.svg')" : "url('/assets/AccessRequestBG.svg')" }}>
         {/* --------------------------------- HEADER --------------------------------- */}
-        <h1 className="font-titilliumWeb-bold text-white text-4xl" style={{ zIndex: 1 }}>
+        <h1 className="font-titilliumWeb-bold text-white text-4xl mt-20" style={{ zIndex: 1 }}>
           Before We Begin:
         </h1>
         {/* --------------------------------- DISCLAIMER BOX --------------------------------- */}
-        <div className="flex-1 w-2/5 h-4/5 bg-gradient-to-b from-nexus100 from-10% to-white to-90% rounded-xl mt-6 p-6" style={{ zIndex: 2 }}>
+        <div className="flex flex-col w-2/5 max-h-4/6 bg-gradient-to-b from-nexus100 from-10% to-white to-90% rounded-xl mt-2 p-6" style={{ zIndex: 2 }}>
           <div className="items-center justify-center flex flex-row">
-            <img src="/assets/Logo.svg" style={{ scale: isMed ? .6 : 1, margin: isMed ? -12 : 24 }} />
-            <img src="/assets/UTDLogo.svg" style={{ scale: isMed ? .6 : 1, margin: isMed ? -12 : 24 }} />
+            <img src="/assets/Logo.svg" style={{ scale: isMed ? .6 : 1, marginInline: isMed ? -12 : 24 }} />
+            <img src="/assets/UTDLogo.svg" style={{ scale: isMed ? .6 : 1, marginInline: isMed ? -12 : 24 }} />
           </div>
           <div className="items-center justify-center flex flex-col" style={{ marginTop: isMed ? -8 : 0, scale: isMed ? .8 : 1 }}>
             <h1 className="font-titilliumWeb-bold text-nexus800 text-5xl">
               Nexus
             </h1>
-            <h2 className="font-titilliumWeb-regular text-center text-3xl">
+            <h2 className="font-titilliumWeb-regular text-nexus800 text-center text-2xl">
               Would like to access your UTD data
             </h2>
           </div>
           {/* --------------------------------- INNER BOX --------------------------------- */}
           <div className="flex flex-col bg-nexus50 rounded-xl items-center text-center justify-center" style={{ marginTop: isMed ? -8 : 16 }}>
-            <h1 className="font-titilliumWeb-semibold text-nexus800 text-3xl pt-4" style={{ fontSize: isMed ? 25 : 30 }}>
+            <h1 className="font-titilliumWeb-bold text-nexus800 text-3xl pt-4" style={{ fontSize: isMed ? 25 : 30 }}>
               This allows Nexus to:
             </h1>
             <h2 className="w-4/5 font-titilliumWeb-regular text-nexus800 text-2xl items-center justify-center text-center"
-              style={{ fontSize: isMed ? 18 : 24 }}>
+              style={{ fontSize: isMed ? 18 : 20 }}>
               Scrape data from your classes in order to partition your classes/role into discord servers.
             </h2>
             <h3 className="w-4/5 font-titilliumWeb-bold text-nexus800 text-2xl items-center justify-center text-center mt-6 mb-4"
-              style={{ fontSize: isMed ? 18 : 24 }}>
+              style={{ fontSize: isMed ? 18 : 20 }}>
               If you want to skip, all you’ll have to do is manually enter in your courses.
             </h3>
           </div>
