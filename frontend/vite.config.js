011cd2dcac1b1c035c63808f8cdf7fa9008e0c08
ryanpolasky <ryanpolasky@hotmail.com>
2026-01-18 02:32:34 -0600

    maybe fix?

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default {
+  plugins: [react()],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
   server: {
     proxy: {
       '/api': {
